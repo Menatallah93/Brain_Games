43440d2ed9544ad828e69c5b9bb1a13443e61280
manar maher <manarmaher835@gmail.com>
2022-12-03 23:42:29 +0200

    Puzzle

---
+++ Manar Maher/audio.js
@@ -0,0 +1,11 @@
+function done() {
+
+
+    window.open("file:///C:/Users/Electronica%20Care/Desktop/project2/indix.html");
+
+}
+
+function playy() {
+    var audio = document.getElementById("audio")
+    audio.play();
+}
